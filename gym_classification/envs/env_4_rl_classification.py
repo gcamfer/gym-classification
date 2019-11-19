b2dc81336e599e893580530452188b6e190d364e
Guillermo Caminero Fernández <guiller5292@gmail.com>
2019-11-20 00:00:58 +0100

    add examples and fix problems

--- gym_classification/envs/env_4_rl_classification.py
+++ gym_classification/envs/env_4_rl_classification.py
@@ -7,12 +7,28 @@ logger = logging.getLogger(__name__)
 
 
 class Env4RLClassification(gym.Env):
+    """
+    Class definition of the Env4RLClassification class that allows to implement 
+    in a simple way a trained classifier with reinforcement learning updates.
+    
+    Parameters
+    ----------
+    X : numpy matrix 
+        Value for x features
+    y : numpy array
+        Target values
+    """
     metadata = {'render.modes': ['none']}
 
-    def __init__(self, X,y,batch_size=1,randomize=False,custom_rewards=None):
+    def __init__(self):
         super(Env4RLClassification, self).__init__()
+        
+    def init_dataset(self, X=None,y=None,batch_size=None,output_shape=None,randomize=False,custom_rewards=None):
+        
 
         self.batch_size = batch_size
+        self.output_shape = output_shape
+
         self.randomize = randomize
         self.custom_rewards = custom_rewards
         self.episode_over = False
@@ -36,16 +52,20 @@ class Env4RLClassification(gym.Env):
         self.X = X
         self.y = y
         
-        self.current_indices = np.arange(batch_size)
+        self.current_indices = np.arange(self.batch_size)
         
     
-    def _reset(self):
+    def reset(self):
         self.episode_over = False
         self.true_labels = np.take(self.y,self.current_indices,axis=0)
-        return np.take(self.X,self.current_indices,axis=0)
+        if self.output_shape:
+            return np.take(self.X,self.current_indices,axis=0).reshape(-1,*self.output_shape)
+        else:
+            return np.take(self.X,self.current_indices,axis=0)
+        
     
     
-    def _step(self, action):
+    def step(self, action):
 
         # Update actions
         self.action = action
@@ -58,21 +78,30 @@ class Env4RLClassification(gym.Env):
           self.episode_over = True
           ## greater
           if last_element == max(self.current_indices):
-            self.current_indices += batch_size
+            self.current_indices += self.batch_size
             dif = max(self.current_indices)-len(self.X)
             self.current_indices[len(self.current_indices)-dif-1:len(self.current_indices)] = list(range(dif+1))
           else:
             self.current_indices = np.arange(last_element+1,last_element+1+self.batch_size,dtype=np.int32)
         else:
-            self.current_indices += batch_size 
+            self.current_indices += self.batch_size 
 
         
         # Update states for next step
         self.true_labels = np.take(self.y,self.current_indices,axis=0)
-        self.status = np.take(self.X,self.current_indices,axis=0)
+        
+        if self.output_shape:
+            self.status = np.take(self.X,self.current_indices,axis=0).reshape(-1,*self.output_shape)
+        else:
+            self.status = np.take(self.X,self.current_indices,axis=0)
+            
+        
         
         return self.status, reward, self.episode_over, {}
 
 
     def _get_reward(self):
         return (self.action == self.true_labels)*1
+        
+    def seed(self):
+        pass
